c7903235472035b8116c7379cb5ca4548cc33683
sukso96100 <sukso96100@gmail.com>
2016-02-11 11:10:06 +0900

    update

--- app/src/main/java/xyz/youngbin/mailtracker/MainActivity.java
+++ app/src/main/java/xyz/youngbin/mailtracker/MainActivity.java
@@ -1,5 +1,7 @@
 package xyz.youngbin.mailtracker;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,7 +11,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import xyz.youngbin.mailtracker.activity.AddNewItemActivity;
+
 public class MainActivity extends AppCompatActivity {
+    Context mContext = MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(mContext, AddNewItemActivity.class));
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
