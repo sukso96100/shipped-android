72e320ed47b3780c560fe1e99e73b9da16387eef
sukso96100 <sukso96100@gmail.com>
2016-02-15 22:23:06 +0900

    working on DataTool

--- app/src/main/java/xyz/youngbin/mailtracker/data/DataTool.java
+++ app/src/main/java/xyz/youngbin/mailtracker/data/DataTool.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.util.Log;
 
 import io.realm.Realm;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
 
 /**
  * Created by youngbin on 16. 2. 10.
@@ -19,8 +21,41 @@ public class DataTool {
     }
 
     public void addNewItem(String Name, String Nat, String Carrier, String Number){
+        if(getItem(Nat, Carrier, Number)==null){
+            mRealm.beginTransaction();
+            MailModel MM = mRealm.createObject(MailModel.class);
+            MM.setName(Name);
+            MM.setNat(Nat);
+            MM.setCarrier(Carrier);
+            MM.setNumber(Number);
+            mRealm.commitTransaction();
+        }
 
     }
 
-    public void checkIfExists(String Nat, String Carrier, String Number){}
+    public void saveItem(MailModel PrevData, String Name, String Nat, String Carrier, String Number){
+        mRealm.beginTransaction();
+        PrevData.setName(Name);
+        PrevData.setNat(Nat);
+        PrevData.setCarrier(Carrier);
+        PrevData.setNumber(Number);
+        mRealm.commitTransaction();
+    }
+
+    public void syncItem(){}
+
+    public MailModel getItem(String Nat, String Carrier, String Number){
+        RealmQuery<MailModel> query = mRealm.where(MailModel.class)
+                .equalTo("Nat",Nat)
+                .equalTo("Carrier",Carrier)
+                .equalTo("Number",Number);
+
+        MailModel result = query.findFirst();
+
+        if(result!=null){
+            return result;
+        }else{
+            return null;
+        }
+    }
 }
